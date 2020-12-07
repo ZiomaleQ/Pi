7cabfd16c9d99549de7ace337948cf00996cbce2
ZiomaleQ <r.partyka30@gmail.com>
2020-12-07 12:56:57 +0100

    New code to test on

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,16 @@
 fun main(args: Array<String>) {
         val tokens = scanTokens(
-        """
+        /* """
     if(n < 1) print(n);
     |else if(n > 1) print(0);
     |print(x);
-    """.trimMargin()
+    """.trimMargin() */
             /* "print(0 + 1 + 2);" */
             /* "if(true) print(0 + 1 + 2); else fuckme();" */
+            /* "print(true < false);" */
+            """while(true) print("XDD");"""
         )
         val temp = Parser(tokens).parse()
-        val temp1 = Checker(temp).parse()
-        val temp2 = Optimizer(temp).parse()
-        for (x in temp2) println(x)
+        val temp1 = Optimizer(temp).parse()
+        for (x in temp1) println(x)
 }
