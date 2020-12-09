9678e061c21c20ed83cf35e539248c2822b1db10
ZiomaleQ <r.partyka30@gmail.com>
2020-12-09 23:23:05 +0100

    removing useless examples and better code

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,6 @@
 fun main( /* args: Array<String> */) {
-
-  /* var poArr = parseCode(""); */
-
-  parseExamples()
+  runExamples()
+  /* parseExamples() */
 }
 
 fun parseCode(code: String, debug: Boolean = false): MutableList<ParserObject> {
@@ -12,10 +10,49 @@ fun parseCode(code: String, debug: Boolean = false): MutableList<ParserObject> {
   return Optimizer(temp).parse()
 }
 
+fun runExamples() {
+  println("\nLet example")
+  println("Code: 'let x = 0;'")
+  Interpreter().run(parseCode("let x = 0;"))
+
+  println("\nReasign example")
+  println("Code: 'let x = 0; x = x + 1; print(x)'")
+  Interpreter().run(parseCode("let x = 0; x = x + 1; print(x);"))
+
+  println("\nIf else example:")
+  println("Code: " + "'if(true) print(0 + 1 + 2); else doSth();'")
+  Interpreter().run(parseCode("if(true) print(0 + 1 + 2); else doSth();"))
+
+  println("\nIdk example:")
+  println("Code: " + "'print(true < false);'")
+  Interpreter().run(parseCode("print(true < false);"))
+
+  println("\nWhile example:")
+  println("Code: " + """'let x = 5; while(x >= 1) { print(x); x = x - 1;}'""")
+  Interpreter().run(parseCode("let x = 5; while(x >= 1) { print(x); x = x - 1;}"))
+
+  println("\nFor example:")
+  println("Code: " + """'for(let x = 0; x < 5; x = x + 1) print(x);'""")
+  Interpreter().run(parseCode("for(let x = 0; x < 5; x = x + 1) print(x);"))
+
+  println("\nPrint with let example:")
+  println("Code: " + "'let x = 0; print(x);'")
+  Interpreter().run(parseCode("let x = 0; print(x);"))
+
+  println("\nFunction example:")
+  println("Code: " + "'fun x(y) {return y + 1;} print(x(0));'")
+  Interpreter().run(parseCode("fun x(y) {return y + 1;} print(x(0));"))
+}
+
 fun parseExamples() {
   println("\nLet example:")
   println("Code: " + "'let x = 0;'")
   var temp = parseCode("let x = 0;")
+  for (x in temp) println(x)
+
+  println("\nReasign example:")
+  println("Code: " + "'let x = 1; x = x + 1; print(x);'")
+   temp = parseCode("let x = 1; x = x + 1; print(x);")
   for (x in temp) println(x)
 
   println("\nIf else example:")
@@ -30,7 +67,7 @@ fun parseExamples() {
 
   println("\nWhile example:")
   println("Code: " + """'while(true) print(x);'""")
-  temp = parseCode("""while(true) print(x);""")
+  temp = parseCode("""let x = 1; while(x >= 1) { print(x); x = x - 1}""")
   for (x in temp) println(x)
 
   println("\nFor example:")
@@ -43,25 +80,8 @@ fun parseExamples() {
   temp = parseCode("let x = 0; print(x);")
   for (x in temp) println(x)
 
-  println("\nReasign example:")
-  println("Code: " + "'let x = 1; x = x + 1; print(x);'")
-   temp = parseCode("let x = 1; x = x + 1; print(x);")
-  for (x in temp) println(x)
-
   println("\nFunction example:")
   println("Code: " + "'fun x(y) {return y + 1;}'")
    temp = parseCode("fun x(y) {return y + 1;}")
   for (x in temp) println(x)
-
-  println("\n2 Let example [ERROR]:")
-  println("Code: " + """'let x = 0; let x = 1;'""")
-  try {
-    temp = parseCode("""let x = 0; let x = 1;""")
-  } catch(err: Error) { println(err.message) }
-
-  println("\nUndefined variable example [ERROR]:")
-  println("Code: " + """'x = x + 1;'""")
-  try {
-    temp = parseCode("""x = x + 1;""")
-  } catch(err: Error) { println(err.message) }
 }
